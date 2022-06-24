3ddfb0a462ee72e8c39f48cc11560e83456cfd3a
matang <matang.hirapara1224@gmail.com>
2022-06-24 15:38:42 +0530

    This is Roller-Box App

---
+++ src/Api.js
@@ -0,0 +1,38 @@
+const result = [
+  {
+    question: 'what is this App name?',
+    answer: ['Calculator', 'Rolldice', 'Todo','Quiz'],
+    correct: 'Quiz',
+    questionId: '#1345'
+  },
+  {
+    question: 'what is this App name?',
+    answer: ['Quiz', 'Rolldice', 'Todo','Calculator'],
+    correct: 'Quiz',
+    questionId: '#1346'
+  },
+  {
+    question: 'what is this App name?',
+    answer: ['Quiz', 'Rolldice', 'Todo','Calculator'],
+    correct: 'Quiz',
+    questionId: '#1347'
+  },
+  {
+    question: 'what is this App name?',
+    answer: ['Quiz', 'Rolldice', 'Todo','Calculator'],
+    correct: 'Quiz',
+    questionId: '#1348'
+  },
+  {
+    question: 'what is this App name?',
+    answer: ['Quiz', 'Rolldice', 'Todo','Calculator'],
+    correct: 'Quiz',
+    questionId: '#1349'
+  },
+  {
+    question: 'what is this App name?',
+    answer: ['Quiz', 'Rolldice', 'Todo','Calculator'],
+    correct: 'Quiz',
+    questionId: '#1350'
+  }
+]
