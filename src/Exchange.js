eb60a7d4663ca6aa53d84739e71e4ccc4e2b7023
matang <matang.hirapara1224@gmail.com>
2022-06-27 14:33:09 +0530

    Exchange-f new update

---
+++ src/Exchange.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Exchange = () => {
+  return (
+    <>
+      <div>
+        <h2>Hello world</h2>
+        <table>
+          <th>
+            <td>First-Name</td>
+            <td>Last-Name</td>
+            <td>Email</td>
+          </th>
+          <tr>
+            <td>Matang</td>
+            <td>Hirapara</td>
+            <td>matang.hirapara12@gmail.com</td>
+          </tr>
+        </table>
+      </div>
+    </>
+  )
+}
+
+export default Exchange
