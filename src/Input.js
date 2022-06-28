fcdc0b9d82f6b2ed427b33d2f02a28c5ea3d4076
matang <matang.hirapara1224@gmail.com>
2022-06-28 16:11:37 +0530

    input: search-bar

---
+++ src/Input.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Input = () => {
+  return (
+    <>
+      <div>
+        <input type="search" placeholder='Search Item....' />
+        <button>Search</button>
+      </div>
+    </>
+  )
+}
+
+export default Input
