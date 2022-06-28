0555a1d5aa11c4502c82ce8f2692328c92a8e079
matang <matang.hirapara1224@gmail.com>
2022-06-28 16:19:02 +0530

    change: new Add Option-bar

--- src/Input.js
+++ src/Input.js
@@ -6,6 +6,16 @@ const Input = () => {
       <div>
         <input type="search" placeholder='Search Item....' />
         <button>Search</button>
+        <div>
+          <select>
+            <option>Surat</option>
+            <option>Banglore</option>
+            <option>Delhi</option>
+            <option>Nagpur</option>
+            <option>Hydrabad</option>
+            <option>Ahemdabad</option>
+          </select>
+        </div>
       </div>
     </>
   )
